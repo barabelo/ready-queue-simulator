ade1149ae62edfa72ce01bfeaf8838141a30b1e5
Bruno <barabelo@hotmail.com>
2021-10-24 23:27:48 -0300

    Adição da classe que modela um processo.

---
+++ src/main/java/br/study/model/Process.java
@@ -0,0 +1,37 @@
+package br.study.model;
+
+public class Process {
+    int pid;
+    int arrivalTime;
+    int burstTime;
+
+    public Process(int pid, int arrivalTime, int burstTime) {
+        this.pid = pid;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+}
