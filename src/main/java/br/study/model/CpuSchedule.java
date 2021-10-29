6ef4ecf78db5990d2f311e9c5cd81084bc6367a9
Bruno <barabelo@hotmail.com>
2021-10-28 22:21:44 -0300

    Adição da classe que modela um cronograma de CPU. Ela aainda está bastante incompleta.

---
+++ src/main/java/br/study/model/CpuSchedule.java
@@ -0,0 +1,11 @@
+package br.study.model;
+
+import java.util.List;
+
+public class CpuSchedule {
+    private List<CpuEvent> cpuEvents;
+
+    public List<CpuEvent> getCpuEvents() {
+        return cpuEvents;
+    }
+}
