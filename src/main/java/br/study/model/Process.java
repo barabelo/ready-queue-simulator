3a42de98d380f4bcbd24eaa406d1477a70d7f948
Bruno <barabelo@hotmail.com>
2021-10-30 19:59:15 -0300

    Refatoração.

--- src/main/java/br/study/model/Process.java
+++ src/main/java/br/study/model/Process.java
@@ -31,12 +31,7 @@ public class Process {
         return burstTime;
     }
 
-    public static Comparator<Process> procArrivalTimeComparator = new Comparator<Process>() {
-        @Override
-        public int compare(Process o1, Process o2) {
-            return o1.getArrivalTime().compareTo(o2.getArrivalTime());
-        }
-    };
+    public static Comparator<Process> procArrivalTimeComparator = Comparator.comparing(Process::getArrivalTime);
 
     @Override
     public boolean equals(Object o) {
