8103ea84b30ca3787ed9c400f730605893423bbb
Bruno <barabelo@hotmail.com>
2021-10-30 00:20:08 -0300

    Adição da classe de testes para a classe CpuSchedule. Precisa ser implementada.

---
+++ src/test/java/br/study/model/TestCpuSchedule.java
@@ -0,0 +1,18 @@
+package br.study.model;
+
+import br.study.model.exception.InvalidDurationException;
+import org.junit.Before;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class TestCpuSchedule {
+
+    @Before
+    public void initialize() throws InvalidDurationException {
+        Process p1 = new Process(1, Instant.ofEpochMilli(0), Duration.ofMillis(1));
+        Process p2 = new Process(2, Instant.ofEpochMilli(0), Duration.ofMillis(2));
+        Process p3 = new Process(3, Instant.ofEpochMilli(0), Duration.ofMillis(3));
+        Process p4 = new Process(4, Instant.ofEpochMilli(0), Duration.ofMillis(4));
+    }
+}
