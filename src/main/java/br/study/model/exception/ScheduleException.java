46b1099b8466092131350ab5a1d0a45e420526e8
Bruno <barabelo@hotmail.com>
2021-10-30 19:58:42 -0300

    Adição de um método à ScheduleException.

--- src/main/java/br/study/model/exception/ScheduleException.java
+++ src/main/java/br/study/model/exception/ScheduleException.java
@@ -4,4 +4,8 @@ public class ScheduleException extends Exception {
     public ScheduleException(String message) {
         super(message);
     }
+
+    public ScheduleException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
