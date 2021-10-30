c1d5f1375edbb60bc627b780ec0dc5955bb201b8
Bruno <barabelo@hotmail.com>
2021-10-30 20:06:37 -0300

    Agora, o método schedule lança uma exceção.

--- src/main/java/br/study/model/CpuScheduler.java
+++ src/main/java/br/study/model/CpuScheduler.java
@@ -1,7 +1,9 @@
 package br.study.model;
 
+import br.study.model.exception.ScheduleException;
+
 import java.util.List;
 
 public interface CpuScheduler {
-    List<CpuTask> schedule(List<Process> processList);
+    List<CpuTask> schedule(List<Process> processList) throws ScheduleException;
 }
