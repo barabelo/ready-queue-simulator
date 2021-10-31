feb1b48a2397a2ecbb4c3ffa2d372413f19a1024
Bruno <barabelo@hotmail.com>
2021-10-31 19:01:46 -0300

    Foi adicionado um teste para o comparador por arrival time da classe Process.

--- src/test/java/br/study/model/TestProcess.java
+++ src/test/java/br/study/model/TestProcess.java
@@ -1,12 +1,19 @@
 package br.study.model;
 
 import br.study.model.exception.InvalidProcessException;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestProcess {
+
+    List<Process> listOfProcessesWithDecreasingArrivalTimes = new ArrayList<>();
+    int lastIndexOfTheList;
 
     @Test(expected = InvalidProcessException.class)
     public void testCreateProcessWithNegativeArrivalTime() throws InvalidProcessException {
@@ -21,5 +28,25 @@ public class TestProcess {
     @Test(expected = InvalidProcessException.class)
     public void testCreateProcessWithNullDuration() throws InvalidProcessException {
         Process processWithNegativeDuration = new Process(1, Instant.ofEpochMilli(1), Duration.ZERO);
+    }
+
+    @Before
+    public void initializeListOfProcessesWithDecreasingArrivalTimes() throws InvalidProcessException {
+        Process processWithTheBiggestArrivalTime = new Process(3, Instant.ofEpochMilli(5), Duration.ofMillis(9));
+        Process processWithTheSmallestArrivalTime = new Process(1, Instant.ofEpochMilli(1), Duration.ofMillis(9));
+        listOfProcessesWithDecreasingArrivalTimes.add(processWithTheBiggestArrivalTime);
+        listOfProcessesWithDecreasingArrivalTimes.add(processWithTheSmallestArrivalTime);
+        lastIndexOfTheList = listOfProcessesWithDecreasingArrivalTimes.size() - 1;
+    }
+
+    @Test
+    public void testProcArrivalTimeComparator() {
+        listOfProcessesWithDecreasingArrivalTimes.sort(Process.procArrivalTimeComparator);
+        Instant arrivalTimeOfTheFirstProcessOnTheSortedProcessList =
+                listOfProcessesWithDecreasingArrivalTimes.get(0).getArrivalTime();
+        Instant arrivalTimeOfTheSecondProcessOnTheSortedProcessList =
+                listOfProcessesWithDecreasingArrivalTimes.get(lastIndexOfTheList).getArrivalTime();
+        Assert.assertTrue(arrivalTimeOfTheFirstProcessOnTheSortedProcessList
+                .isBefore(arrivalTimeOfTheSecondProcessOnTheSortedProcessList));
     }
 }
