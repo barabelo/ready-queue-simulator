9fe17fcab45f7a609c24de9c60382c1e2edd687c
Bruno <barabelo@hotmail.com>
2021-11-01 14:03:14 -0300

    Remoção de métodos da classe CpuSchedule e remoção da exceção ScheduleException.

--- src/main/java/br/study/model/CpuSchedule.java
+++ src/main/java/br/study/model/CpuSchedule.java
@@ -1,7 +1,5 @@
 package br.study.model;
 
-import br.study.model.exception.ScheduleException;
-
 import java.util.List;
 
 public class CpuSchedule {
@@ -9,30 +7,6 @@ public class CpuSchedule {
 
     public List<CpuTask> getCpuEvents() {
         return cpuTasks;
-    }
-
-    public void scheduleEvent(CpuTask cpuTask) throws ScheduleException {
-        if (alreadyScheduled(cpuTask)) throw new ScheduleException("This CPU event has already been " +
-                "scheduled.");
-        if (hasConflict(cpuTask)) throw new ScheduleException("There is already a CPU scheduled at this time" +
-                ".");
-        cpuTasks.add(cpuTask);
-    }
-
-    public boolean hasConflict(CpuTask newEvent) {
-        for (CpuTask event : cpuTasks) {
-            if ((newEvent.getStart().isAfter(event.getStart()) && newEvent.getStart().isBefore(event.getEnd()))
-                    || (newEvent.getEnd().isAfter(event.getStart()) && newEvent.getEnd().isBefore(event.getEnd())))
-                return true;
-        }
-        return false;
-    }
-
-    public boolean alreadyScheduled(CpuTask newEvent) {
-        for (CpuTask event : cpuTasks) {
-            if (newEvent.equals(event)) return true;
-        }
-        return false;
     }
 
 }
--- src/main/java/br/study/model/exception/ScheduleException.java
+++
@@ -1,8 +0,0 @@
-package br.study.model.exception;
-
-public class ScheduleException extends Exception {
-
-    public ScheduleException(String message) {
-        super(message);
-    }
-}
