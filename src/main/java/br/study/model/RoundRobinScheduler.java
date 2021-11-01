ad925b6456535467be6b14f80344fbdb68bf42f2
Bruno <barabelo@hotmail.com>
2021-11-01 14:54:47 -0300

    Mudança no tipo do atributo quantum, remoção do setter e começo da implementação do método schedule.

--- src/main/java/br/study/model/RoundRobinScheduler.java
+++ src/main/java/br/study/model/RoundRobinScheduler.java
@@ -1,26 +1,23 @@
 package br.study.model;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RoundRobinScheduler implements CpuScheduler {
 
-    private int quantum;
+    private Duration quantum;
 
-    public RoundRobinScheduler(int quantum) {
-        if (quantum == 0) throw new IllegalArgumentException("The quantum value cannot be zero.");
-        if (quantum < 0) throw new IllegalArgumentException("The quantum value cannot be negative.");
-        this.quantum = quantum;
-    }
-
-    public void setQuantum(int quantum) {
-        if (quantum == 0) throw new IllegalArgumentException("The quantum value cannot be zero.");
-        if (quantum < 0) throw new IllegalArgumentException("The quantum value cannot be negative.");
+    public RoundRobinScheduler(Duration quantum) {
+        if (quantum.isZero()) throw new IllegalArgumentException("The quantum value cannot be zero.");
+        if (quantum.isNegative()) throw new IllegalArgumentException("The quantum value cannot be negative.");
         this.quantum = quantum;
     }
 
     @Override
     public List<CpuTask> schedule(List<Process> processList) {
-
+        processList.sort(Process.procArrivalTimeComparator);
+        List<CpuTask> cpuTasks = new ArrayList<>();
         return null;
     }
 }
